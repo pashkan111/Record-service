598ab7c2aae938f2c2879004538acf28b103917d
pashkan111 <kozhevnikov0001@yandex.ru>
2022-03-08 23:40:26 +0300

    Changed name from Data to Record

--- src/service_app/models.py
+++ src/service_app/models.py
@@ -4,14 +4,14 @@ from sqlalchemy.dialects.postgresql import JSON
 from .schemas import DataPostSchema
 
 
-class Data(Base):
-    __tablename__ = 'data'
+class Record(Base):
+    __tablename__ = 'record'
     id = sa.Column(sa.Integer, primary_key=True)
     key = sa.Column(sa.String(64))
     data = sa.Column(JSON)
     
     @classmethod
-    def add_data(cls, data: DataPostSchema, key: str) -> str:
+    def add_record(cls, data: DataPostSchema, key: str) -> str:
         new_record = cls(key=key, data=data)
         session.add(new_record)
         session.commit()

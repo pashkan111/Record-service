f620ba9c71b5fc5309e6ecaace8e5279d081d8a8
pashkan111 <kozhevnikov0001@yandex.ru>
2022-03-08 23:39:49 +0300

    Added func for creating new record

--- src/service_app/models.py
+++ src/service_app/models.py
@@ -1,6 +1,7 @@
 import sqlalchemy as sa
-from db.db import Base
+from db.db import Base, session
 from sqlalchemy.dialects.postgresql import JSON
+from .schemas import DataPostSchema
 
 
 class Data(Base):
@@ -8,3 +9,11 @@ class Data(Base):
     id = sa.Column(sa.Integer, primary_key=True)
     key = sa.Column(sa.String(64))
     data = sa.Column(JSON)
+    
+    @classmethod
+    def add_data(cls, data: DataPostSchema, key: str) -> str:
+        new_record = cls(key=key, data=data)
+        session.add(new_record)
+        session.commit()
+        return key
+    
